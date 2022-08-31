925d6ee1d349e0401ecd650864015f86e96bea24
pavel.cse.buet <shaownakter.1995@gmail.com>
2022-08-31 13:07:17 +0600

    database with laravel

---
+++ resources/views/blog/add.blade.php
@@ -0,0 +1,57 @@
+@extends('master')
+
+@section('title')
+    Add Blog Page
+@endsection
+
+@section('body')
+    <section class="py-5 bg-info">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Add Blog Form</h4>
+                        </div>
+                        <div class="card-body">
+                            <p class="text-center text-success">{{Session::get('message')}}</p>
+                            <form action="{{route('blog.new')}}" method="POST">
+                                @csrf
+                                <div class="row mb-3">
+                                    <label for="" class="col-md-3">Blog Title</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control" name="title"/>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <label for="" class="col-md-3">Author Name</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control" name="author"/>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <label for="" class="col-md-3">Blog Description</label>
+                                    <div class="col-md-9">
+                                        <textarea name="description" class="form-control"></textarea>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <label for="" class="col-md-3">Blog Image</label>
+                                    <div class="col-md-9">
+                                        <input type="file" class="form-control" name="image"/>
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <label for="" class="col-md-3"></label>
+                                    <div class="col-md-9">
+                                        <input type="submit" class="btn btn-outline-success px-5" name="btn" value="Create New Blog"/>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
